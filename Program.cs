476d78a3c4728b231bc40e47d5449813985716de
Михаил Нефидов <mis5919@yandex.ru>
2018-05-19 15:20:54 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Net.NetworkInformation;
+
+namespace ChatClient
+{
+    class Program
+    {
+        static string userName;
+        //private static string host = GetIPAddress();
+        private const int port = 8888;
+        static TcpClient client;
+        static NetworkStream stream;
+        public static string DomainName { get { return IPGlobalProperties.GetIPGlobalProperties().DomainName; } }
+        public static string DnsIp { get { return GetDnsIp(); } }
+
+        public static string GetDomainIP()
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(DomainName);
+            string host = System.Net.Dns.GetHostName();
+            IPHostEntry Hast = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (System.Net.IPAddress ip in Hast.AddressList)
+            {
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            return host;
+        }
+
+        public static string GetDnsIp()
+        {
+            NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface networkInterface in networkInterfaces)
+            {
+                if (networkInterface.OperationalStatus == OperationalStatus.Up)
+                {
+                    IPInterfaceProperties ipProp = networkInterface.GetIPProperties();
+                    IPAddressCollection dnsAddresses = ipProp.DnsAddresses;
+                    foreach (IPAddress dnsAdress in dnsAddresses)
+                    {
+                        return dnsAdress.ToString();
+                    }
+                }
+            }
+            throw new InvalidOperationException("Unable to find DNS Address");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Domain Name: {0}", DomainName);
+            Console.WriteLine(GetDnsIp());
+            Console.Write("Введите свое имя: ");
+            userName = Console.ReadLine();
+            client = new TcpClient();
+            string ipaddr = DnsIp;
+            try
+            {
+                client.Connect(ipaddr, port); //подключение клиента
+                stream = client.GetStream(); // получаем поток
+
+                string message = userName;
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                // запускаем новый поток для получения данных
+                Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+                receiveThread.Start(); //старт потока
+                Console.WriteLine("Добро пожаловать, {0}", userName);
+                SendMessage();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+        // отправка сообщений
+        static void SendMessage()
+        {
+            Console.WriteLine("Введите сообщение: ");
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+            }
+        }
+        // получение сообщений
+        static void ReceiveMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] data = new byte[64]; // буфер для получаемых данных
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+                    Console.WriteLine(message);//вывод сообщения
+                }
+                catch
+                {
+                    Console.WriteLine("Подключение прервано!"); //соединение было прервано
+                    Console.ReadLine();
+                    Disconnect();
+                }
+            }
+        }
+
+        static void Disconnect()
+        {
+            if (stream != null)
+                stream.Close();//отключение потока
+            if (client != null)
+                client.Close();//отключение клиента
+            Environment.Exit(0); //завершение процесса
+        }
+    }
+}
